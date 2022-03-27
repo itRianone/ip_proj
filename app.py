bae34e53fd8d541ab78b2926abc0a9c68016dbfb
itRianone <timokhindimitry@gmail.com>
2022-03-27 18:18:06 +0300

    add victorin

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from config import app, db, quizes, descriptions
+from config import app, db
+from data import quizes, descriptions,victs
 from flask import Flask, render_template, request, redirect, url_for, flash, session, make_response
 from models import User
 from sqlalchemy.exc import IntegrityError as sql_error
@@ -15,24 +16,31 @@ def main():
     username = session['username']
   except:
     username = None
-  print(username)
+  #print(username)
   return render_template('home.html', username=username)
 
-@app.route('/<quizname>')
-def quiz_page(quizname):
-  print(quizname)
+@app.route('/<quiz_type>/<quizname>')
+def quiz_page(quiz_type, quizname):
+  print(quiz_type, quizname)
   try:
     description = descriptions[quizname]
   except:
     description=''
-  return render_template('quiz_page.html', description=description, quizname=quizname)
+  return render_template('quiz_page.html', quiz_type=quiz_type, description=description, quizname=quizname)
 
 
-# @app.errorhandler(400)
-@app.route('/<quizname>/<question_num>', methods=["GET", "POST"])
+@app.errorhandler(Exception)
+def err(e):
+  return render_template('err.html', error=e)
+
+def nearest(lst, target):
+  return min(lst, key=lambda x: abs(x-target))
+
+
+@app.route('/test/<quizname>/<question_num>', methods=["GET", "POST"])
 def quiz(quizname, question_num):
   if request.method == "POST":
-    
+    #print(quiz_type)
     try:
       req = request.form
 
@@ -88,6 +96,69 @@ def quiz(quizname, question_num):
   
   except KeyError:
     return "<body style='background-color:black;color:white;'>no such quiz</body>"
+
+@app.route('/victorin/<quizname>/<question_num>', methods=["GET", "POST"])
+def victorin(quizname, question_num):
+  if request.method == "POST":
+    #print(quiz_type)
+    try:
+      req = request.form
+
+      #balls = []
+      right = list(victs[quizname]['questions'].values())[int(question_num)-1]
+      opt = victs[quizname]['options'][int(question_num)-1]
+      balls=zip(right.split(),opt)
+      # print(right, [i for i in balls])
+      #cache.set(f'{question_num}', f'{req["option"]}')
+      for i in balls:
+        #print(1, req["option"], i[1])
+        if req["option"] == i[1]:
+          session[f'{question_num}'] = i[0]
+          print(i[0], i[1], req["option"])
+
+
+      #print(req['option'], list(victs[quizname]['questions'].values())[int(question_num)-1])
+    except:
+      error = 'Выбери что нибудь'
+      return render_template('question.html', quest=list(victs[quizname]['questions'].keys())[int(question_num)-1],
+                                          opts=victs[quizname]['options'][int(question_num)-1],
+                                          quizname=quizname,
+                                          error=error)
+
+    return redirect(url_for('victorin', quizname=quizname, question_num=int(question_num)+1))
+
+
+  try:
+    #print(list(quizes[quizname]['questions'].keys())[int(question_num)],quizes[quizname]['options'][int(question_num)-1])
+    return render_template('question.html', quest=list(victs[quizname]['questions'].keys())[int(question_num)-1],
+                                        opts=victs[quizname]['options'][int(question_num)-1],
+                                        quizname=quizname)
+  except IndexError:
+    balls_count = 0
+    for i in range(1, int(question_num)):
+
+      # q = cache.get(str(i))
+      try:
+        answer = session[f'{i}']
+        if answer is None:
+          continue
+        
+        #print(answer,list(victs[quizname]['questions'].values())[int(i)-1])
+        #if str(answer) == list(victs[quizname]['questions'].values())[int(i)-1]:
+        balls_count += int(answer)
+        end_variants_values = list(victs[quizname]['variants'].values())
+        #end_variants_values = [int(x) for x in end_variants_values]
+        near = nearest(end_variants_values, balls_count)
+        #list(victs[quizname]['variants'].keys())
+        res_of_victorin = list(victs[quizname]['variants'].keys())[list(victs[quizname]['variants'].values()).index(near)]
+
+      except KeyError:
+        pass
+    return render_template('end_vict.html', quest_amount=int(question_num)-1, res_of_victorin=res_of_victorin)
+  
+  # except KeyError:
+  #   return "<body style='background-color:black;color:white;'>no such quiz</body>"
+
 
 @app.route('/logout', methods=['POST', 'GET']) 
 def logout():
