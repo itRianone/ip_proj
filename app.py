ac43a51bb8bdccf392f11396a53ae1de765c4e4b
itRianone <timokhindimitry@gmail.com>
2022-03-29 23:39:58 +0300

    new design and whatever

--- app.py
+++ app.py
@@ -1,26 +1,25 @@
-from config import app, db,UPLOAD_FOLDER, ALLOWED_EXTENSIONS
+from config import app, db, UPLOAD_FOLDER, ALLOWED_EXTENSIONS, cache
 from data import quizes,victs
 from flask import Flask, render_template, request, redirect, url_for, flash, session, make_response
 from models import User
 from sqlalchemy.exc import IntegrityError as sql_error
 import os
 
-db.create_all()
-
 # tell Flask to use the above defined config
 
-
+# @cache.cached(timeout=500)
 @app.route('/')
 def main():
   # username = request.cookies.get('username')
   try:
     user_name = session['username']
     username = User.query.filter_by(username=str(session['username'])).one()
-    pic=username.profile_picture
-    return render_template('home.html', username=user_name, pic=pic)
+    session['prof_pic'] = username.profile_picture
+    pic = username.profile_picture
+    return render_template('home.html', pic=pic)
   except:
     username = None
-    return render_template('home.html', username=username)
+    return render_template('home.html')
 
   #print(username)
 
@@ -41,7 +40,7 @@ def quiz_page(quiz_type, quizname):
 def nearest(lst, target):
   return min(lst, key=lambda x: abs(x-target))
 
-
+# @cache.cached(timeout=500)
 @app.route('/test/<quizname>/<question_num>', methods=["GET", "POST"])
 def quiz(quizname, question_num):
   if request.method == "POST":
@@ -57,7 +56,8 @@ def quiz(quizname, question_num):
       return render_template('question.html', quest=list(quizes[quizname]['questions'].keys())[int(question_num)-1],
                                           opts=quizes[quizname]['options'][int(question_num)-1],
                                           quizname=quizname,
-                                          error=error)
+                                          error=error,
+                                          question_num=question_num)
 
     return redirect(url_for('quiz', quizname=quizname, question_num=int(question_num)+1))
 
@@ -67,7 +67,8 @@ def quiz(quizname, question_num):
     #print(list(quizes[quizname]['questions'].keys())[int(question_num)],quizes[quizname]['options'][int(question_num)-1])
     return render_template('question.html', quest=list(quizes[quizname]['questions'].keys())[int(question_num)-1],
                                         opts=quizes[quizname]['options'][int(question_num)-1],
-                                        quizname=quizname)
+                                        quizname=quizname,
+                                        question_num=question_num)
   except IndexError:
     correct_answers_count = 0
     answers = []
@@ -102,6 +103,7 @@ def quiz(quizname, question_num):
   except KeyError:
     return "<body style='background-color:black;color:white;'>no such quiz</body>"
 
+# @cache.cached(timeout=500)
 @app.route('/victorin/<quizname>/<question_num>', methods=["GET", "POST"])
 def victorin(quizname, question_num):
   if request.method == "POST":
@@ -128,7 +130,8 @@ def victorin(quizname, question_num):
       return render_template('question.html', quest=list(victs[quizname]['questions'].keys())[int(question_num)-1],
                                           opts=victs[quizname]['options'][int(question_num)-1],
                                           quizname=quizname,
-                                          error=error)
+                                          error=error,
+                                          question_num=question_num)
 
     return redirect(url_for('victorin', quizname=quizname, question_num=int(question_num)+1))
 
@@ -137,7 +140,8 @@ def victorin(quizname, question_num):
     #print(list(quizes[quizname]['questions'].keys())[int(question_num)],quizes[quizname]['options'][int(question_num)-1])
     return render_template('question.html', quest=list(victs[quizname]['questions'].keys())[int(question_num)-1],
                                         opts=victs[quizname]['options'][int(question_num)-1],
-                                        quizname=quizname)
+                                        quizname=quizname,
+                                        question_num=question_num)
   except IndexError:
     balls_count = 0
     for i in range(1, int(question_num)):
@@ -178,27 +182,28 @@ def logout():
 # @app.errorhandler(500)
 @app.route('/login', methods=['POST', 'GET']) 
 def login():
-  if request.method == 'POST':
-    data = request.form
-    username = data['login_name']
-    password = data['login_passw']
-    
-    user = User.query.filter_by(username=username).first()
-    print(user)
+  if not session['username']:
+    if request.method == 'POST':
+      data = request.form
+      username = data['login_name']
+      password = data['login_passw']
       
-    if user and password == user.password:
-      session['username'] = username
-      return redirect(url_for('main'))
+      user = User.query.filter_by(username=username).first()
+      print(user)
+        
+      if user and password == user.password:
+        session['username'] = username
+        return redirect(url_for('main'))
 
-    else:
-      error = 'нет такого либо неверный пароль' 
-      return render_template('login.html', error=error) 
-    # resp = make_response()
-    # resp.set_cookie('username', username)
+      else:
+        error = 'нет такого либо неверный пароль' 
+        return render_template('login.html', error=error) 
+      # resp = make_response()
+      # resp.set_cookie('username', username)
 
 
-  return render_template('login.html')      
-
+    return render_template('login.html')      
+  return redirect(url_for('main'))
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
@@ -217,39 +222,43 @@ def profile():
         #image_name = secure_filename(file.filename) ????
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
         username.profile_picture = file.filename
+        session['prof_pic']=username.profile_picture
         db.session.commit()
         return render_template('lk.html', pic=username.profile_picture)#
     return render_template('lk.html', pic=username.profile_picture)
   return redirect(url_for('registration'))
+
 # @app.errorhandler(500)
 @app.route('/registration', methods=['POST', 'GET']) 
 def registration():
-  if request.method == 'POST':
-    data = request.form
-    username = data['reg_name']
-    password = data['reg_passw']
-    if len(username) >= 5 and len(password) >= 5:
-      print('ok go')
-    else:
-        
-      error ='Имя и пароль минимум по 5 символов'
-      return render_template('registration.html', error=error)
+  if not session['username']:
+    if request.method == 'POST':
+      data = request.form
+      username = data['reg_name']
+      password = data['reg_passw']
+      if len(username) >= 5 and len(password) >= 5:
+        print('ok go')
+      else:
+          
+        error ='Имя и пароль минимум по 5 символов'
+        return render_template('registration.html', error=error)
 
-    try:
-      user = User(username=username, password=password,profile_picture='no')
-      db.session.add(user)
-      db.session.commit()
+      try:
+        user = User(username=username, password=password,profile_picture=None)
+        db.session.add(user)
+        db.session.commit()
 
-      session['username'] = username
-      # resp = make_response()
-      # resp.set_cookie('username', username)
+        session['username'] = username
+        # resp = make_response()
+        # resp.set_cookie('username', username)
 
-      return redirect(url_for('main'))
-    except sql_error as e:
-      print(e)
-      error = 'Уже есть в бд такой юзер'
-      return render_template('registration.html', error=error)
-  return render_template('registration.html') 
+        return redirect(url_for('main'))
+      except sql_error as e:
+        print(e)
+        error = 'Уже есть в бд такой юзер'
+        return render_template('registration.html', error=error)
+    return render_template('registration.html') 
+  return redirect(url_for('main'))
 
 if __name__=="__main__":
   app.run()
